9382e94f123e1411698035029068b20fc32e46d3
Bill Ying <yingzhor@gmail.com>
2015-09-08 14:36:38 +0800

    update

--- spring-auto-qiniuyun/src/main/java/com/github/yingzhuo/spring/auto/qiniuyun/QiniuyunConfiguration.java
+++ spring-auto-qiniuyun/src/main/java/com/github/yingzhuo/spring/auto/qiniuyun/QiniuyunConfiguration.java
@@ -3,6 +3,8 @@ package com.github.yingzhuo.spring.auto.qiniuyun;
 import com.github.yingzhuo.spring.auto.qiniuyun.core.QiniuyunManager;
 import com.github.yingzhuo.spring.auto.qiniuyun.core.SimpleQiniuyunManager;
 import com.qiniu.util.Auth;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -17,8 +19,14 @@ import org.springframework.core.annotation.Order;
 @ConditionalOnProperty(name = "spring.auto.qiniuyun.enabled", havingValue = "true", matchIfMissing = false)
 public class QiniuyunConfiguration {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(QiniuyunConfiguration.class);
+
     @Autowired
     private ConfigBean configBean;
+
+    public QiniuyunConfiguration() {
+        LOGGER.debug("spring-auto: '{}' enabled.", QiniuyunConfiguration.class.getSimpleName());
+    }
 
     @Bean
     public QiniuyunManager qiniuyunUploadManager() {
